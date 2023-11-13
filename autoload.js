f7e067bd57bc3ece44ef69932c8f4420f4e29222
dl58 <dl58@illinois.edu>
2023-11-13 00:57:38 -0600

    self api

--- autoload.js
+++ autoload.js
@@ -35,8 +35,8 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
-			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "quit"]
+			cdnPath: "https://fastly.jsdelivr.net/gh/rathgrith/live2d_api/",
+			tools: ["asteroids", "switch-model", "switch-texture", "photo", "quit"]
 		});
 	});
 }
